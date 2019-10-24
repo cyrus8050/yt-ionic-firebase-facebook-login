013d95112fbff0f007e1192a60ba62de29074413
Cyrus Chan <cyruschan@hsu.edu.hk>
2019-10-24 16:59:02 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
 
+
+import * as firebase from 'firebase/app';
+// You don't need to import firebase/app either since it's being imported above
+import 'firebase/auth';
+import 'firebase/firestore';
+var firebaseConfig = {
+  apiKey: "AIzaSyCtPn2bLRRXabK-DBCMyL0iDx4YVDwHfqU",
+  authDomain: "facebooklogin-46b8c.firebaseapp.com",
+  databaseURL: "https://facebooklogin-46b8c.firebaseio.com",
+  projectId: "facebooklogin-46b8c",
+  storageBucket: "facebooklogin-46b8c.appspot.com",
+  messagingSenderId: "373854862482",
+  appId: "1:373854862482:web:a3437315a2ea6f8c7fba4b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +33,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Facebook,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app';
+// You don't need to import firebase/app either since it's being imported above
+import 'firebase/auth';
+import 'firebase/firestore';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +12,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private fb:Facebook) {}
+  login(){
+    this.fb.login(['email'])
+  .then((res: FacebookLoginResponse) => {
+    let credential = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken)
+    firebase.auth().signInWithCredential(credential).then(info=>{
+      alert(JSON.stringify(info))
+    })
+  })
+  .catch(e => console.log('Error logging into Facebook', e));
+    // var provider = new firebase.auth.FacebookAuthProvider();
+    // firebase.auth().signInWithRedirect(provider).then(()=>{
+    //   firebase.auth().getRedirectResult().then(function(result) {
+    //     alert(result)
+    //   }).catch(function(error) {
+    //     // Handle Errors here.
+    //     alert(JSON.stringify(error))
+    //     // ...
+    //   });
+    // })
 
+  }
 }
